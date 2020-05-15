e71a6644d7a8eacd83fbbd6ada735b9a49fb36af
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-05-15 16:13:41 +0800

    Updates

---
+++ precache-manifest.b52febbbab5b3aafc8298dcc56e8f647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c8ebaa76129b810ff93dd9b84f67ec",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "312da89b1b04b28b0d70",
+    "url": "/monsters-rolodex/static/css/main.6e008693.chunk.css"
+  },
+  {
+    "revision": "1b163c6dfb305b2c587f",
+    "url": "/monsters-rolodex/static/js/2.b0e7918a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b0e7918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312da89b1b04b28b0d70",
+    "url": "/monsters-rolodex/static/js/main.989fd51c.chunk.js"
+  },
+  {
+    "revision": "df5d6ef2e44460cbab72",
+    "url": "/monsters-rolodex/static/js/runtime-main.51942013.js"
+  }
+]);
